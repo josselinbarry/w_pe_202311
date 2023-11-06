4fede29aa3b7fc85f1006f17bd8c284d32134248
josselinbarry <josselin.barry@ofb.gouv.fr>
2023-11-06 11:15:45 +0100

    bon ...

--- w_pe_202311.R
+++ w_pe_202311.R
@@ -18,11 +18,17 @@ source(file = "R/functions.R")
 
 ## import des données ----
 
+# pe <- 
+#   sf::read_sf(dsn = "data/plando_full_source_2311.gpkg")
+
 pe <- 
-  sf::read_sf(dsn = "data/plando_full_source_2311.gpkg")
+  sf::read_sf(dsn = "data/ech_pe.gpkg")
+
+# qa <- 
+# sf::read_sf(dsn = "data/qA_zone_etude.gpkg")
 
 qa <- 
-sf::read_sf(dsn = "data/qA_zone_etude.gpkg")
+  sf::read_sf(dsn = "data/ech_qa.gpkg")
 
 q5 <- 
 sf::read_sf(dsn = "data/q5_zone_etude.gpkg")
@@ -37,3 +43,7 @@ dist <- st_distance(pe, qa[plus_proches_troncons,], by_element = TRUE)
 pe_cd_carthage <- pe %>% 
   cbind(dist) %>% 
   cbind(qa[plus_proches_troncons,]) 
+
+## export des résultats ----
+
+sf::write_sf(obj = pe_cd_carthage, dsn = "data/outputs/plando_idcarth_202311.gpkg")
